97d5e755009b295fe1cdfba3335bd8be2e2ee6ba
Alvaro Andrade <varito1@gmail.com>
2021-01-13 20:23:53 -0300

    Added landing page sites components. Added firebase admin

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,39 @@
-import '../styles/globals.css'
+import { ThemeProvider, CSSReset } from '@chakra-ui/react';
+import { Global, css } from '@emotion/react';
+
+import { AuthProvider } from '@/lib/auth';
+import theme from '@/styles/theme';
+
+const GlobalStyle = ({ children }) => (
+  <>
+    <CSSReset />
+    <Global
+      styles={css`
+        html {
+          min-width: 360px;
+          scroll-behavior: smooth;
+        }
+
+        #__next {
+          display: flex;
+          flex-direction: column;
+          min-height: 100vh;
+        }
+      `}
+    />
+    {children}
+  </>
+);
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <AuthProvider>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
